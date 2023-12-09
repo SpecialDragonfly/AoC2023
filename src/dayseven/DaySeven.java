4e9697fd5f8a060a7a0226f0685211d94060e3ac
Dominic Orme <Dominic.Orme@googlemail.com>
2023-12-09 21:32:35 +0000

    Completed Day 7

--- src/dayseven/DaySeven.java
+++ src/dayseven/DaySeven.java
@@ -9,8 +9,8 @@ import java.util.stream.Collectors;
 import util.FileReader;
 
 public class DaySeven {
-	@SuppressWarnings("unchecked")
-	public static void main(String[] args) {
+	
+	private static void runPartOne() {
 		Vector<String> lines = FileReader.readFile("./src/dayseven/input.txt");
 		
 		HashMap<String, Integer> cardRanking = new HashMap<String, Integer>();
@@ -50,10 +50,66 @@ public class DaySeven {
 		int total = 0;
 		for (int i = 0; i < hands.size(); i++) {
 			Hand h = hands.get(i);
-			int winning = hands.get(i).getBid() * (i + 1);
+			int winning = h.getBid() * (i + 1);
 			total += winning;
-			System.out.println(hands.get(i) + " Winning : " + winning);
+			System.out.println(h + " Winning : " + winning);
 		}
 		System.out.println("Answer: " + total);
+	}
+	
+	private static void runPartTwo() {
+		Vector<String> lines = FileReader.readFile("./src/dayseven/input.txt");
+		
+		HashMap<String, Integer> cardRanking = new HashMap<String, Integer>();
+		// A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
+		cardRanking.put("A", 14);
+		cardRanking.put("K", 13);
+		cardRanking.put("Q", 12);
+		cardRanking.put("J", -1);
+		cardRanking.put("T", 10);
+		cardRanking.put("9", 9);
+		cardRanking.put("8", 8);
+		cardRanking.put("7", 7);
+		cardRanking.put("6", 6);
+		cardRanking.put("5", 5);
+		cardRanking.put("4", 4);
+		cardRanking.put("3", 3);
+		cardRanking.put("2", 2);
+		
+		Vector<AltHand> hands = new Vector<AltHand>();
+		lines.stream().forEach(line -> {
+			String[] parts = line.trim().split(" ");
+			List<String> cards = List.of(parts[0].split(""));
+			String bid = parts[1];
+			
+			hands.add(
+				new AltHand(
+					cards.stream().map(c -> {
+						return new Card(cardRanking.get(c), c);
+					}).collect(Collectors.toList()), 
+					bid
+				)
+			);
+		});
+		
+		Collections.sort(hands);
+		
+		int total = 0;
+		for (int i = 0; i < hands.size(); i++) {
+			AltHand h = hands.get(i);
+			int winning = h.getBid() * (i + 1);
+			total += winning;
+			System.out.println(h + " Winning : " + winning);
+		}
+		System.out.println("Answer: " + total);
+	
+		// 254412181
+
+	}
+
+	
+	public static void main(String[] args) {
+		DaySeven.runPartOne();
+		DaySeven.runPartTwo();
 	}
 }
--- src/dayseven/handtypes/HandTypeFactory.java
+++ src/dayseven/handtypes/HandTypeFactory.java
@@ -7,12 +7,76 @@ import dayseven.Card;
 
 public class HandTypeFactory {
 	private static HashMap<String, Integer> numbers = new HashMap<>();
+	
+	public static HandTypeInterface getTypeWithJoker(Vector<Card> cards) {
+		HandTypeFactory.populate(cards);
+		// Do we have any jokers?
+		if (numbers.containsKey("J")) {
+			// How many do we have?
+			// If we have 1 joker - then the lowest hand is a pair
+			// If we have 2 jokers - then the lowest hand is 3 of a kind
+			// If we have 3 jokers - then the lowest hand is 4 of a kind
+			// If we have 4 jokers - then the lowest hand is 5 of a kind
+			int quantity = numbers.get("J");
+			numbers.remove("J"); // Don't consider J as a valid 'number' for the purposes of jokers.
+			if (quantity == 5) {
+				return new FiveOfAKind();
+			}
+			if (quantity == 4) {
+				return new FiveOfAKind();
+			}
+			if (quantity == 3) {
+				if (numbers.containsValue(2)) {
+					return new FiveOfAKind();
+				}
+				return new FourOfAKind();
+			}
+			// So quantity is now 2 or 1
+			if (quantity == 2) {
+				if (numbers.containsValue(3)) {
+					return new FiveOfAKind();	
+				}
+				if (numbers.containsValue(2)) {
+					return new FourOfAKind();
+				}
+				// We can't return a full house with 2 jokers, because the 3 would become 5.
+				// Alternatively the 4 would be 5 of a kind.
+				return new ThreeOfAKind();
+			} else if (quantity == 1) {
+				if (numbers.containsValue(4)) {
+					return new FiveOfAKind();
+				}
+				if (numbers.containsValue(3)) {
+					return new FourOfAKind();
+				}
+				if (numbers.containsValue(2)) {
+					// How many do we have?
+					if (numbers.values().stream().filter(v -> v == 2).toList().size() == 2) {
+						return new FullHouse();
+					} else {
+						return new ThreeOfAKind();
+					}
+				}
+			}
+			return new OnePair();
+		}
+		return HandTypeFactory.get();
+	}
+	
 	public static HandTypeInterface getType(Vector<Card> cards) {
+		HandTypeFactory.populate(cards);
+		return HandTypeFactory.get();
+	}
+	
+	private static void populate(Vector<Card> cards) {
 		numbers.clear();
 		cards.forEach(c -> {
 			int currentValue = numbers.getOrDefault(c.getFaceValue(), 0);
 			numbers.put(c.getFaceValue(), currentValue + 1);
 		});
+	}
+	
+	private static HandTypeInterface get() {
 		if (numbers.containsValue(5)) {
 			return new FiveOfAKind();
 		}
@@ -33,7 +97,6 @@ public class HandTypeFactory {
 				return new OnePair();
 			}
 		}
-		return new HighCard();
-		
+		return new HighCard();		
 	}
 }
