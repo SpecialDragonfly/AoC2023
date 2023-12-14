9bff8575c9f5a0791aaafd9fcc60bfbedc4cd0a2
Dominic Orme <Dominic.Orme@googlemail.com>
2023-12-14 22:10:25 +0000

    Fixed! Also formatting issues.

--- src/daythirteen/DayThirteen.java
+++ src/daythirteen/DayThirteen.java
@@ -5,70 +5,61 @@ import java.util.Vector;
 import util.FileReader;
 
 public class DayThirteen {
-	
-	private static void runPartOne() {
-		Vector<String> lines = FileReader.readFile("./src/daythirteen/test4.txt");
-		Vector<Map> maps = new Vector<>();
-		Map m = new Map();
-		for (int i = 0; i < lines.size(); i++) {
-			if (lines.get(i).equals("")) {
-				maps.add(m);
-				m = new Map();
-				continue;
-			}
-			m.addRow(lines.get(i));
-		}
-		maps.add(m);
-		
-		System.out.println(m.toString());
-		
-		int sum = 0;
-		for (int i = 0; i < maps.size(); i++) {
-			System.out.println("Map: " + i);
-			int a = maps.get(i).getHorizontalValue();
-			System.out.println(a > 0 ? "\tHorizontal: " + a : "");
-			int b = maps.get(i).getVerticalValues();
-			System.out.println(b > 0 ? "\tVertical: " + b : "");
-			sum += a + b;
-		}
-		System.out.println(sum);
-	}
-	
-	public static void main(String[] args) {
-	    //DayThirteen.runPartOne();
-		DayThirteen.runPartTwo();
-	}
-	
-	public static void runPartTwo() {
-		Vector<String> lines = FileReader.readFile("./src/daythirteen/test4.txt");
-		Vector<AdvancedMap> maps = new Vector<>();
-		AdvancedMap m = new AdvancedMap();
-		for (int i = 0; i < lines.size(); i++) {
-			if (lines.get(i).equals("")) {
-				maps.add(m);
-				m = new AdvancedMap();
-				continue;
-			}
-			m.addRow(lines.get(i));
-		}
-		maps.add(m);
-		
-		//System.out.println(m);
-		
-		int sum = 0;
-		for (int i = 0; i < maps.size(); i++) {
-			System.out.println("Map: " + i);
-			int a = maps.get(i).getHorizontalValue();
-			System.out.println(a > 0 ? "\tHorizontal: " + a : "");
-			int b = maps.get(i).getVerticalValues();
-			System.out.println(b > 0 ? "\tVertical: " + b : "");
-			sum += a + b;
-		}
-		System.out.println(sum);
+    
+    private static void runPartOne() {
+        Vector<String> lines = FileReader.readFile("./src/daythirteen/input.txt");
+        Vector<Map> maps = new Vector<>();
+        Map m = new Map();
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).equals("")) {
+                maps.add(m);
+                m = new Map();
+                continue;
+            }
+            m.addRow(lines.get(i));
+        }
+        maps.add(m);
+        
+        int sum = 0;
+        for (int i = 0; i < maps.size(); i++) {
+            int a = maps.get(i).getHorizontalValue();
+            int b = maps.get(i).getVerticalValues();
+            sum += a + b;
+        }
+        System.out.println(sum);
+    }
+    
+    public static void main(String[] args) {
+        DayThirteen.runPartOne();
+        DayThirteen.runPartTwo();
+    }
+    
+    public static void runPartTwo() {
+        Vector<String> lines = FileReader.readFile("./src/daythirteen/input.txt");
+        Vector<AdvancedMap> maps = new Vector<>();
+        AdvancedMap m = new AdvancedMap();
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).equals("")) {
+                maps.add(m);
+                m = new AdvancedMap();
+                continue;
+            }
+            m.addRow(lines.get(i));
+        }
+        maps.add(m);
+        
+        int sum = 0;
+        for (int i = 0; i < maps.size(); i++) {
+            int a = maps.get(i).getHorizontalValue();
+            int b = maps.get(i).getVerticalValues();
+            sum += a + b;
+        }
+        System.out.println(sum);
 
-		// 30063 -> too high
-		// 30005 -> too high
-		// 31363 -> too high	
-		// 13996 -> not even close
-	}
+        // 30063 -> too high
+        // 30005 -> too high
+        // 31363 -> too high    
+        // 13996 -> not even close
+        // Answer: 22906
+    }
 }
--- src/daythirteen/Map.java
+++ src/daythirteen/Map.java
@@ -3,94 +3,94 @@ package daythirteen;
 import java.util.Vector;
 
 public class Map {
-	Vector<String> rows = new Vector<>();
-	public Map() {
-		
-	}
+    Vector<String> rows = new Vector<>();
+    public Map() {
+        
+    }
 
-	public void addRow(String l) {
-		this.rows.add(l);
-	}
-	
-	public int getVerticalValues() {
-		int colCount = this.rows.get(0).length();
-		for (int i = 0; i < colCount - 1; i++) {
-			if (this.compareColumn(i, i+1)) {
-				if (this.checkReflectedColumns(i)) {
-					return i+1;
-				}
-			}
-		}
-		return 0;
-	}
-	
-	public int getHorizontalValue() {
-		for (int i = 0; i < rows.size() - 1; i++) {
-			if (this.rows.get(i).equals(this.rows.get(i+1))) {
-				if (this.checkReflectedRows(i)) {
-					return 100*(i+1);
-				}
-			}
-		}
-		
-		return 0;
-	}
-	
-	private boolean checkReflectedColumns(int i) {
-		int maxSize = this.rows.get(0).length();
-		for (int start = 1; start <= i; start++) {
-			if (i - start < 0) {
-				break;
-			}
-			if (i + start + 1 >= maxSize) {
-				break;
-			}
-			if (!this.compareColumn(i - start, i + start + 1)) {
-				return false;
-			}
-		}
+    public void addRow(String l) {
+        this.rows.add(l);
+    }
+    
+    public int getVerticalValues() {
+        int colCount = this.rows.get(0).length();
+        for (int i = 0; i < colCount - 1; i++) {
+            if (this.compareColumn(i, i+1)) {
+                if (this.checkReflectedColumns(i)) {
+                    return i+1;
+                }
+            }
+        }
+        return 0;
+    }
+    
+    public int getHorizontalValue() {
+        for (int i = 0; i < rows.size() - 1; i++) {
+            if (this.rows.get(i).equals(this.rows.get(i+1))) {
+                if (this.checkReflectedRows(i)) {
+                    return 100*(i+1);
+                }
+            }
+        }
+        
+        return 0;
+    }
+    
+    private boolean checkReflectedColumns(int i) {
+        int maxSize = this.rows.get(0).length();
+        for (int start = 1; start <= i; start++) {
+            if (i - start < 0) {
+                break;
+            }
+            if (i + start + 1 >= maxSize) {
+                break;
+            }
+            if (!this.compareColumn(i - start, i + start + 1)) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	private boolean compareColumn(int columnOneIdx, int columnTwoIdx) {
-		String columnOne = "";
-		String columnTwo = "";
-		for (int i = 0; i < this.rows.size(); i++) {
-			columnOne += String.valueOf(this.rows.get(i).charAt(columnOneIdx));
-			columnTwo += String.valueOf(this.rows.get(i).charAt(columnTwoIdx));
-		}
-		return columnOne.equals(columnTwo);
-	}
-	
-	/*
-	 * 1 2 3 4 5 6 7 8
-	 * 4 comp 5 (i = 3, start = 0)
-	 * 3(2) comp 6(5) (i = 3, start = 1) comp i -start, i+start+1
-	 * 2(1) comp 7(6) (i=3, start = 2) comp i-start, i+
-	 */
-	
-	private boolean checkReflectedRows(int i) {
-		for (int start = 1; start <= i; start++) {
-			if (i - start < 0) {
-				break;
-			}
-			if (i + start + 1 >= this.rows.size()) {
-				break;
-			}
-			if (!this.rows.get(i - start).equals(this.rows.get(i + start + 1))) {
-				return false;	
-			}
-		}
+    private boolean compareColumn(int columnOneIdx, int columnTwoIdx) {
+        String columnOne = "";
+        String columnTwo = "";
+        for (int i = 0; i < this.rows.size(); i++) {
+            columnOne += String.valueOf(this.rows.get(i).charAt(columnOneIdx));
+            columnTwo += String.valueOf(this.rows.get(i).charAt(columnTwoIdx));
+        }
+        return columnOne.equals(columnTwo);
+    }
+    
+    /*
+     * 1 2 3 4 5 6 7 8
+     * 4 comp 5 (i = 3, start = 0)
+     * 3(2) comp 6(5) (i = 3, start = 1) comp i -start, i+start+1
+     * 2(1) comp 7(6) (i=3, start = 2) comp i-start, i+
+     */
+    
+    private boolean checkReflectedRows(int i) {
+        for (int start = 1; start <= i; start++) {
+            if (i - start < 0) {
+                break;
+            }
+            if (i + start + 1 >= this.rows.size()) {
+                break;
+            }
+            if (!this.rows.get(i - start).equals(this.rows.get(i + start + 1))) {
+                return false;    
+            }
+        }
 
-		return true;
-	}
-	
-	public String toString() {
-		String x = "";
-		for (int i = 0; i < this.rows.size(); i++) {
-			x += this.rows.get(i).toString() + "\n";
-		}
-		return x;
-	}
+        return true;
+    }
+    
+    public String toString() {
+        String x = "";
+        for (int i = 0; i < this.rows.size(); i++) {
+            x += this.rows.get(i).toString() + "\n";
+        }
+        return x;
+    }
 }
